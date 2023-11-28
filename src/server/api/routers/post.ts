18b5ec3d4ae50c5e4ab32749903b866bab88872a
Igor <egor@sgf.lt>
2023-11-28 17:28:57 +0200

    Change title

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -29,7 +29,7 @@ export const postRouter = createTRPCRouter({
     }),
 
   getLatest: publicProcedure.query(({ ctx }) => {
-    return ctx.db.query.posts.findFirst({
+    return ctx.db.query.posts.findMany({
       orderBy: (posts, { desc }) => [desc(posts.createdAt)],
     });
   }),
