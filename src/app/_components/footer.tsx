3272a882fe5a6b39144f357b30eb7a89982c3ff1
Igor <egor@sgf.lt>
2023-12-17 21:26:53 +0200

    Increase footer margin

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="flex flex-col items-center justify-center gap-4 py-8">
+    <footer className="mt-16 flex flex-col items-center justify-center gap-4 py-8">
       <div className="flex gap-8">
         <a href="/about">About Us</a>
         <a href="/contact">Contact</a>
