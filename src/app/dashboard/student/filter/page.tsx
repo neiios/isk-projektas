7318609edc375039a963c435ed9366228790a3ba
Igor <egor@sgf.lt>
2023-12-18 11:27:21 +0200

    Add search page

---
+++ src/app/dashboard/student/filter/page.tsx
@@ -0,0 +1,61 @@
+import { api } from "~/trpc/server";
+
+export default async function Page() {
+  const tutors = await api.users.getAvailableTutors.query();
+  const subjects = await api.subjects.getSubjects.query();
+
+  return (
+    <div className="mt-16">
+      <h2 className="mb-8 text-3xl font-bold">Susirask korepetitorių</h2>
+      <table className="border-separate border border-black shadow-sharp [border-spacing:0.75rem]">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Subjects</th>
+            <th>Languages</th>
+            <th>Study Types</th>
+            <th>Hourly Rate</th>
+          </tr>
+          <tr>
+            <form action="">
+              <th>Name</th>
+              <th>Description</th>
+              <th>Subjects</th>
+              <th>Languages</th>
+              <th>Study Types</th>
+              <th>Hourly Rate</th>
+            </form>
+          </tr>
+        </thead>
+        <tbody>
+          {tutors.map((tutor) => (
+            <tr key={tutor.id}>
+              <td>{tutor.name}</td>
+              <td>{tutor.description}</td>
+              <td>
+                {tutor.subjects
+                  .map((subject) => subjects[subject.subjectId - 1]?.name)
+                  .join(", ")}
+              </td>
+              <td>
+                {tutor.languages
+                  .map((language) => language.language)
+                  .join(", ")}
+              </td>
+              <td>
+                {tutor.studyTypes.map((type) => type.studyType).join(", ")}
+              </td>
+              <td>{tutor.pricePerHour}</td>
+              <td>
+                <button className="border border-black p-2 shadow-sharp">
+                  Rezervuoti
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
--- src/app/dashboard/student/page.tsx
+++ src/app/dashboard/student/page.tsx
@@ -9,6 +9,7 @@ import { api } from "~/trpc/server";
 import { getInitials } from "~/utils/helpers";
 
 import { reservations as reservationsTable } from "~/server/db/schema";
+import Link from "next/link";
 
 export default async function Page() {
   const session = await getServerAuthSession();
@@ -144,45 +145,17 @@ export default async function Page() {
             </div>
           ))}
         </div>
-      </div>
-
-      <div className="mt-16">
-        <h2 className="mb-8 text-3xl font-bold">Susirask korepetitorių</h2>
-        <table className="border-separate border border-black shadow-sharp [border-spacing:0.75rem]">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Description</th>
-              <th>Subjects</th>
-              <th>Languages</th>
-              <th>Study Types</th>
-              <th>Hourly Rate</th>
-            </tr>
-          </thead>
-          <tbody>
-            {tutors.map((tutor) => (
-              <tr key={tutor.id}>
-                <td>{tutor.name}</td>
-                <td>{tutor.description}</td>
-                <td>
-                  {tutor.subjects
-                    .map((subject) => subjects[subject.subjectId - 1]?.name)
-                    .join(", ")}
-                </td>
-                <td>
-                  {tutor.languages
-                    .map((language) => language.language)
-                    .join(", ")}
-                </td>
-                <td>
-                  {tutor.studyTypes.map((type) => type.studyType).join(", ")}
-                </td>
-                <td>{tutor.pricePerHour}</td>
-                <td>Rezervuoti</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <div className="mt-16 flex flex-col items-center">
+          <h2 className="mb-8 text-3xl font-bold">
+            Not finding what you need?
+          </h2>
+          <Link
+            className="border border-black px-16 py-6 shadow-sharp"
+            href="/dashboard/student/filter"
+          >
+            Try advanced search
+          </Link>
+        </div>
       </div>
     </div>
   );
